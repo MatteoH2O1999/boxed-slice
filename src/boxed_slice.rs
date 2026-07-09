dc2139c65cc1ee1c1911509c43da2e7b9b99b47e
Matteo Dell'Acqua <matteodellacqua@ibilab.it>
2026-07-09 11:44:22 +0200

    Implement helpers

--- src/boxed_slice.rs
+++ src/boxed_slice.rs
@@ -1,12 +1,48 @@
 use alloc::boxed::Box;
-use core::ops::{Deref, DerefMut};
+use core::{
+    mem::{forget, MaybeUninit},
+    ops::{Deref, DerefMut},
+};
+
+struct PanicGuard<'a, T> {
+    slice: &'a mut [MaybeUninit<T>],
+    initialized: usize,
+}
+
+impl<'a, T> Drop for PanicGuard<'a, T> {
+    fn drop(&mut self) {
+        let initialized_part = &mut self.slice[..self.initialized];
+        // SAFETY: this raw sub-slice will contain only initialized objects.
+        unsafe {
+            initialized_part.assume_init_drop();
+        }
+    }
+}
 
 pub fn new_boxed_slice<T: Default>(len: usize) -> Box<[T]> {
     new_boxed_slice_with_initializer(T::default, len)
 }
 
 pub fn new_boxed_slice_with_value<T: Clone>(value: T, len: usize) -> Box<[T]> {
-    todo!()
+    let mut uninit_box = Box::new_uninit_slice(len);
+    {
+        let mut guard = PanicGuard {
+            slice: &mut uninit_box,
+            initialized: 0,
+        };
+        if let Some((last, elems)) = guard.slice.split_last_mut() {
+            for elem in elems {
+                elem.write(value.clone());
+                guard.initialized += 1;
+            }
+            last.write(value);
+        }
+        forget(guard);
+    }
+    unsafe {
+        // Safety: we just wrote len valid elements
+        uninit_box.assume_init()
+    }
 }
 
 pub fn new_boxed_slice_with_initializer<T>(mut func: impl FnMut() -> T, len: usize) -> Box<[T]> {
@@ -14,10 +50,25 @@ pub fn new_boxed_slice_with_initializer<T>(mut func: impl FnMut() -> T, len: usi
 }
 
 pub fn new_boxed_slice_with_indexed_initializer<T>(
-    func: impl FnMut(usize) -> T,
+    mut func: impl FnMut(usize) -> T,
     len: usize,
 ) -> Box<[T]> {
-    todo!()
+    let mut uninit_box = Box::new_uninit_slice(len);
+    {
+        let mut guard = PanicGuard {
+            slice: &mut uninit_box,
+            initialized: 0,
+        };
+        for elem in guard.slice.iter_mut() {
+            elem.write(func(guard.initialized));
+            guard.initialized += 1;
+        }
+        forget(guard);
+    }
+    unsafe {
+        // Safety: we just wrote len valid elements
+        uninit_box.assume_init()
+    }
 }
 
 #[repr(transparent)]
@@ -53,10 +104,6 @@ impl<T> BoxedSlice<T> {
     #[inline]
     pub fn into_inner(self) -> Box<[T]> {
         self.0
-    }
-    #[inline]
-    pub fn into_box(self) -> Box<[T]> {
-        self.into_inner()
     }
 }
 
